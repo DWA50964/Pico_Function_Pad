37de8afe49efba181e173cb7df8d7539eca3da40
40325292 <dandrew01@qub.ac.uk>
2022-02-22 00:39:36 +0000

    Initial Code Commit
    Some of the basic test code I have been running on the version 1 Device with 3 modes.
    
    -Number pad (1)
    -F13 - F22 (2)
    -LED Tests (10)

---
+++ src/code.py
@@ -0,0 +1,155 @@
+import time
+
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+#Setup Keyboard HID Device
+time.sleep(1)
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+
+#Led Setup
+led1 = digitalio.DigitalInOut(board.GP2)
+led2 = digitalio.DigitalInOut(board.GP8)
+led3 = digitalio.DigitalInOut(board.GP28)
+led4 = digitalio.DigitalInOut(board.GP5)
+led5 = digitalio.DigitalInOut(board.GP3)
+led6 = digitalio.DigitalInOut(board.GP26)
+led7 = digitalio.DigitalInOut(board.GP15)
+
+leds_List = [led1, led2, led3, led4, led5, led6, led7]
+for led in leds_List:
+    led.direction = digitalio.Direction.OUTPUT
+
+
+#Button Setup
+btn1 = digitalio.DigitalInOut(board.GP11)
+btn2 = digitalio.DigitalInOut(board.GP10)
+btn3 = digitalio.DigitalInOut(board.GP22)
+btn4 = digitalio.DigitalInOut(board.GP12)
+btn5 = digitalio.DigitalInOut(board.GP20)
+btn6 = digitalio.DigitalInOut(board.GP19)
+btn7 = digitalio.DigitalInOut(board.GP14)
+btn8 = digitalio.DigitalInOut(board.GP13)
+btn9 = digitalio.DigitalInOut(board.GP18)
+btn10 = digitalio.DigitalInOut(board.GP17)
+
+btnFn = digitalio.DigitalInOut(board.GP16)
+btnFn.direction = digitalio.Direction.INPUT
+btnFn.pull = digitalio.Pull.UP
+
+btns_List = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10]
+for btn in btns_List:
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+
+
+
+
+global mode
+global modeList
+global functionKeys
+
+#List of all modes in use
+modeList = [0,1,2,10]
+
+functionKeys = [Keycode.F13, Keycode.F14,Keycode.F15, Keycode.F16, Keycode.F17, Keycode.F18, Keycode.F19,Keycode.F20, Keycode.F21, Keycode.F22]
+
+def keyPress(key):
+    global mode
+    global modeList
+
+    #Number pad mode
+    if mode == 1:
+        if int(key) < 10:
+            keyboard_layout.write(str(key))
+        else:
+            keyboard_layout.write("0")
+        time.sleep(0.1)
+
+    #Functions Keys Mode
+    if mode == 2:
+        keyboard.press(functionKeys[int(key)-1])
+        keyboard.release_all()
+        time.sleep(0.1)
+
+    #Led Test Mode
+    if mode == 10:
+        for led in leds_List:
+            led.value = True
+            time.sleep(0.2)
+        for led in leds_List:
+            led.value = False
+            time.sleep(0.2)
+
+    #Mode Selection mode
+    if mode == 0:
+        if int(key) in modeList:
+            mode = int(key)
+            #Blinks Leds to mark Selected mode
+            for i in range(mode):
+                for led in leds_List:
+                    led.value = True
+                time.sleep(0.2)
+                for led in leds_List:
+                    led.value = False
+                time.sleep(0.2)
+
+
+#Boot Lights
+for led in leds_List:
+    led.value = True
+    time.sleep(0.2)
+for led in leds_List:
+    led.value = False
+    time.sleep(0.2)
+
+#Default mode
+mode = 1
+count = 0
+
+#Main Program Loop
+while True:
+
+    #Checks for button presses
+    for btn in btns_List:
+        if not btn.value:
+            keyPress(str(btns_List.index(btn) + 1))
+
+
+    #Checks for function button presses
+    if not btnFn.value:
+        led7.value = True
+        count += 1
+
+    #If function button is tapped, blink to show modes
+    else:
+        led7.value = False
+        if count > 10:
+            time.sleep(0.4)
+            for i in range(mode):
+                led7.value = True
+                time.sleep(0.2)
+                led7.value = False
+                time.sleep(0.2)
+        count = 0
+
+    #If function button is held enter mode selection
+    if count == 3000:
+        mode = 0
+        led7.value = False
+        time.sleep(1)
+        for i in range(2):
+            for led in leds_List:
+                led.value = True
+            time.sleep(0.2)
+            for led in leds_List:
+                led.value = False
+            time.sleep(0.2)
+        count = 0
+
+
